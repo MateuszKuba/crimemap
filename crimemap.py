e981778cf151022c36af747d93ad2744bc4d8dbf
MateuszKuba <mateusz.kuba@onet.eu>
2017-07-23 00:33:18 +0200

    crime map v2 ( adding )

--- crimemap.py
+++ crimemap.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jul 22 16:09:17 2017
@@ -9,7 +10,7 @@ from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
-
+import json
 
 app = Flask(__name__)
 DB = DBHelper()
@@ -17,29 +18,21 @@ DB = DBHelper()
 
 @app.route("/")
 def home():
-    try:
-        data = DB.get_all_inputs()
-    except Exception as e:
-        print e
-        data = None
-    return render_template("home.html", data=data)
+     crimes = DB.get_all_crimes()
+     crimes = json.dumps(crimes)
+     return render_template("home.html", crimes=crimes)
 
-@app.route("/add", methods=["POST"])
-def add():
-  try:
-    data = request.form.get("userinput")
-    DB.add_input(data)
-  except Exception as e:
-    print e
-  return home()
 
-@app.route("/clear")
-def clear():
-  try:
-    DB.clear_all()
-  except Exception as e:
-    print e
-  return home()
+@app.route("/submitcrime",methods=["POST"])
+def submitcrime():
+    category = request.form.get("category")
+    date = request.form.get("date")
+    latitude = float(request.form.get("latitude"))
+    longitude = float(request.form.get("longitude"))
+    description = request.form.get("description")
+    DB.add_crime(category, date, latitude, longitude, description)
+    return home()
+
 
 
 if __name__ == "__main__":
